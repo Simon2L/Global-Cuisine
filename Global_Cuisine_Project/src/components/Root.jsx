8af503689de649841d7c3b17b7e0f655bf314f52
Zander <alexander.backlin.salas@iths.se>
2023-04-18 13:23:17 +0200

    importerade createBrowserm RouterProvider.

---
+++ Global_Cuisine_Project/src/components/Root.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Root() {
+  return (
+    <div>Hello</div>
+  )
+}
+
+export default Root
--- Global_Cuisine_Project/src/main.jsx
+++ Global_Cuisine_Project/src/main.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import Root from './components/Root'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+//#region sätter upp Routing
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />
+  },
+]);
+//#endregion
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>,
 )
