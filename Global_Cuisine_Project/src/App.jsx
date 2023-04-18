3034b67cfa1e6e90b635592a85f1cddcfb495818
HarunMohamed2022 <112860947+HarunMohamed2022@users.noreply.github.com>
2023-04-18 15:37:47 +0200

    Create navbar

--- Global_Cuisine_Project/src/App.jsx
+++ Global_Cuisine_Project/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Navbar from './components/NavBar/navbar'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <div>Hello</div>
+    <Navbar></Navbar>
   )
 }
 
