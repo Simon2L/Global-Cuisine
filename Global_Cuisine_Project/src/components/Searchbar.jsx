ab3a7af37c5d077efabd7966c9f434bba9eec551
Simon2L <frasselk@gmail.com>
2023-04-20 14:54:55 +0200

    Searchbar without function

--- Global_Cuisine_Project/src/components/Searchbar.jsx
+++ Global_Cuisine_Project/src/components/Searchbar.jsx
@@ -1,6 +1,5 @@
 import './Searchbar.css'
-import search_alt from '../images/Search_alt.png'
-import filter from '../images/filter-image.png'
+import { FaSearch, FaAlignLeft } from 'react-icons/fa'
 
 
 const Searchbar = () => {
@@ -10,9 +9,9 @@ const Searchbar = () => {
             <h1 className="search-form__title">Global Cuisine</h1>
             <p className="search-form__subtitle">connects you to 2.3 millions recipes spread over 500+ websites</p>
             <form className="search-form__container">
-                <input className="search-form__input" data-="text" placeholder="Search..."/>
-                <button className="search-form__submit"><img src={search_alt}></img></button>
-                <button className="search-form__filter"><img src={filter}></img></button>
+                <input className="search-form__input" maxLength={25} data-="text" placeholder="Search..."/>
+                <FaSearch className="search-form__submit" />
+                <button className="search-form__filter"> <FaAlignLeft /></button>
             </form>
         </section>
         </>
