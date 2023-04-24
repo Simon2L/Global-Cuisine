cd6d5359b46a4973b6ad4780bebf8f2144be3ec6
Simon2L <frasselk@gmail.com>
2023-04-24 12:53:47 +0200

    test

--- Global_Cuisine_Project/src/components/Searchbar.jsx
+++ Global_Cuisine_Project/src/components/Searchbar.jsx
@@ -3,9 +3,11 @@ import { FaSearch, FaAlignLeft } from 'react-icons/fa'
 import logoimage from '../assets/logo-no-background.svg';
 
 const handleSubmit = (e) => {
-    console.log(e)
     e.preventDefault();
-    getRecipes();
+    const data = new FormData(e.target);
+    data.set('search', data.get('search'));
+    console.log(data);
+    //getRecipes();
   };
 
 const getRecipes = async (search) => {
@@ -29,7 +31,7 @@ const Searchbar = () => {
             <p className="search-form__subtitle">connects you to 2.3 millions recipes spread over 500+ websites</p> */}
             <img src={logoimage} alt="global cuisine" className='logoimage'></img>
             <form className="search-form__container" onSubmit={handleSubmit}>
-                <input className="search-form__input" maxLength={25} data-="text" placeholder="Search..."/>
+                <input id="search" name="search" className="search-form__input" maxLength={25} data-="text" placeholder="Search..."/>
                 <FaSearch className="search-form__submit" />
                 <button className="search-form__filter"> <FaAlignLeft /></button>
             </form>
