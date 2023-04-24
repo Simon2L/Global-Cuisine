035e855afdda20815b3f6f3ff27fe50178d8773a
Simon2L <frasselk@gmail.com>
2023-04-24 12:24:34 +0200

    handlesubmit

--- Global_Cuisine_Project/src/components/Searchbar.jsx
+++ Global_Cuisine_Project/src/components/Searchbar.jsx
@@ -2,6 +2,25 @@ import './Searchbar.css'
 import { FaSearch, FaAlignLeft } from 'react-icons/fa'
 import logoimage from '../assets/logo-no-background.svg';
 
+const handleSubmit = (e) => {
+    console.log(e)
+    e.preventDefault();
+    getRecipes();
+  };
+
+const getRecipes = async (search) => {
+    //const apiKey = 'ca68133f5df34a13b64e53f977918ba8';
+    try {
+        const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&query=${search}`;
+        const response = await fetch(url);
+        const result = await response.json();
+        console.log(result);
+        //setRecipes(result);
+      } catch (e) {
+        console.log(e);
+      }
+}
+
 const Searchbar = () => {
     return(
         <>
@@ -9,7 +28,7 @@ const Searchbar = () => {
             {/* <h1 className="search-form__title">Global Cuisine</h1>
             <p className="search-form__subtitle">connects you to 2.3 millions recipes spread over 500+ websites</p> */}
             <img src={logoimage} alt="global cuisine" className='logoimage'></img>
-            <form className="search-form__container">
+            <form className="search-form__container" onSubmit={handleSubmit}>
                 <input className="search-form__input" maxLength={25} data-="text" placeholder="Search..."/>
                 <FaSearch className="search-form__submit" />
                 <button className="search-form__filter"> <FaAlignLeft /></button>
