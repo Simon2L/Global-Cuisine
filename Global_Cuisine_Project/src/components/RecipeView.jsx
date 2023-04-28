74aa6be6266d312fb4fda65a54d4a19dea936bfb
Luca Ionut-Catalin <ic.luca@outlook.com>
2023-04-28 09:55:56 +0200

    fixed recipeview css and cleaned code

--- Global_Cuisine_Project/src/components/RecipeView.jsx
+++ Global_Cuisine_Project/src/components/RecipeView.jsx
@@ -3,128 +3,94 @@ import './RecipeView.css'
 import recipeImage from '../assets/brooke.jpg'
 import { useLocation } from 'react-router-dom'
 import { useState,useEffect } from 'react';
-
-function ShowRecipeInfo(props) {
-
-    const location = useLocation();
-    const data = location.state.recipe;
-    console.log(data)
-    // const [recipe,setRecipe] = useState();
-    // const getRecipes = async () => {
-    // const apiKey = 'f27d562bd85b4cd5a482eb0b9108beeb';
-    // try {
-    //     const url = `https://api.spoonacular.com/recipes/${data}/information?apiKey=${apiKey}`;
-    //     const response = await fetch(url);
-    //     const result = await response.json();               
-    //     setRecipe(result);
-    //     console.log(result);
-    //   } catch (e) {
-    //     console.log(e);
-    //   }
-    // }
-    // useEffect(() => {
-    //     getRecipes();
-    // },[])
-    
-    return (  
-       <>
-        <div className='recipe-info'>        
-            <div className="recipe-title">
-                <h2>{data.title}</h2>  
-                </div>          
-                <div className='recipe-title'>
-                    <img src={data.image} alt="recipe image" 
-                    className='recipe-img' ></img>
-                </div>
-            
-            <div className="recipe-ingredients"> 
-                <div className='ingredients'>           
-                    <h3 className='title'>Ingredients : </h3> 
-                    <hr className='solid' />           
-                <div>
-                    <ul className='list'>
-                        {data.extendedIngredients.map((ingredient) =>
-                            <li className='list-element'>{ingredient.original}</li>
-                        )}                        
-                    </ul>
-                    </div>
-                    </div>
-                <div className='instructions' >            
-                    <h3 className='title'>Instructions : </h3> 
-                    <hr className='solid' />            
-                <div>
-                    <ol className='list2'>
-                        {data.analyzedInstructions.map((instruction,index) => {
-                            return <div key={index} >
-                                {instruction.steps.map((step) => <li key={step.number}>
-                                    <span className='step'>{step.step}</span></li>)}
-                            </div>
-                            
-                           } 
-                        )}                        
-                    </ol>
-                </div>
-            </div>
-                
-            </div>
-            
-        </div>        
-        
-        </>
-        
-       
-    )
-}
-
-
-function NutritionInfo() {
-    return (
-        <>
-        <h2>Nutritions Facts : </h2>
-            <div >
-                <ul>
-                        <li>Ingredient 1</li>
-                        <li>Ingredient 2</li>
-                        <li>Ingredient 3</li>
-                        <li>Ingredient 4</li>
-                        <li>Ingredient 5</li>
-                    </ul>
-            </div>
-        </>
-    )
-}
+import parse from 'html-react-parser'
 
 
 export default function RecipeView() {
-   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+    const location = useLocation();
+    const data = location.state.recipe;  //gets the recipe object from home page
+    const summary = '<h3>'+data.summary.split('. ',1)+'</h3>'; // splits and saves the first sentence of summary atribute of the recipe
+    
 
-  useEffect(() => {
-    const handleWindowResize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-
-    window.addEventListener('resize', handleWindowResize);
-
-    return () => {
-      window.removeEventListener('resize', handleWindowResize);
-    };
-  });
-
-    if(windowWidth >=728)
         return (
-            <div className="recipe-container-desktop">
-                <ShowRecipeInfo></ShowRecipeInfo>
+            
+            <div className="recipe-container">
+                <div className='recipe-info'>
+                           
+                    <div className="title-container"> 
+                        <h1>{data.title}</h1>
+                        <div className='title-info'>                       
+                            <div className='image-container'>                   
+                            <img src={data.image} alt="recipe image" 
+                            className='recipe-img' ></img>
+                            </div>
+                            <div className='summary'>                            
+                                <span>{parse(summary)}</span>
+                                <div>
+                                    <div>
+                                        <h5>Time to prepare</h5>
+                                        <h4>{data.readyInMinutes} minutes</h4>
+                                    </div> 
+                                    <div>
+                                        <h5>Servings</h5>
+                                        <h4>{data.servings} portions</h4>
+                                    </div> 
+                                    <div>
+                                        <h6>Health score</h6>
+                                        <h4 className='last'>{data.healthScore} points</h4>
+                                    </div>   
+                                </div>                            
+                            </div>
+                        </div> 
+                    </div>
+                
+
+
+                <div className="recipe-ingredients"> 
+                    <div className='ingredients'>           
+                        <h2 className=''>Ingredients : </h2> 
+                        <hr className='solid' />           
+                    <div>
+                        <ul className='list'>
+                            {data.extendedIngredients.map((ingredient) =>
+                                <li key={ingredient.id} className='list-element'>{ingredient.original}</li>
+                            )}                        
+                        </ul>
+                        </div>
+                        </div>
+                    <div className='instructions' >            
+                        <h2 className=''>Instructions : </h2> 
+                        <hr className='solid' />            
+                    <div>
+                        <ol className='list'>
+                            {data.analyzedInstructions.map((instruction,index) => {
+                                return <div key={index} >
+                                    {instruction.steps.map((step) => <li key={step.number}>
+                                        <span>{step.step}</span></li>)}
+                                </div>
+                                
+                            } 
+                            )}                        
+                        </ol>
+                    </div>
+                </div>                
+            </div>
+            </div> 
                 <div className="nutrition-info">
-                    <NutritionInfo></NutritionInfo>
+                    <h2>Nutritions Facts : </h2>
+                    <hr className='solid'></hr>                    
+                    <p>Serving size :<span>
+                        {data.nutrition.weightPerServing.amount}{data.nutrition.weightPerServing.unit}</span></p>
+                    
+                    
+                    <div >
+                        <ul>
+                              {/* {data.}   */}
+                            </ul>
+                    </div>
                 </div>
             </div>
+            
         )
-    else return(
-        <div className="recipe-container-mobile">
-                <ShowRecipeInfo></ShowRecipeInfo>
-                 <div className="nutrition-info-mobile">
-                    <NutritionInfo></NutritionInfo>
-                </div>
-            </div>
-    )
+   
 }
