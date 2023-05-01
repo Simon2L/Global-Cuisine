edb69894de03d13e6c34b72d223356d57041e576
Zander <alexander.backlin.salas@iths.se>
2023-05-01 08:48:47 +0200

    kan lägga in array med filters på api search

--- Global_Cuisine_Project/src/components/Searchbar.jsx
+++ Global_Cuisine_Project/src/components/Searchbar.jsx
@@ -3,6 +3,7 @@ import { FaSearch, FaAlignLeft } from 'react-icons/fa'
 import logoimage from '../assets/logo-no-background.svg';
 import { useState } from 'react';
 import FilterMenu from './SearchBar/FilterMenu';
+import { Form } from "react-router-dom";
 
 
 
@@ -14,38 +15,35 @@ const Searchbar = (props) => {
     const [intoleranceOption, setIntoleranceOption] = useState([])
     const [search, setSearch] = useState("");
    
-    const [region, setRegion] = useState([["Italian,", false], ["European", false]])
-    let mealType = [["breakfast,", false], ["main course", false]]
-    let diet = [["Vegetarian,", false], ["Vegan", false]]
-    let intolerance = [["Gluten,", false], ["Dairy", false]]
+    let region = [""] 
+    let mealType = ["breakfast"]
+    let diet = ["Vegan"]
+    let intolerance = ["Gluten", "Dairy"]
 
-    let diets = ""
-    diet.forEach(item => {
-        if(item[1] === true) test += item[0]
+   const PrintFilters = (array) => {
+    let filterString = ""
+    array.forEach(element => {
+        filterString += `${element}, `
     });
 
-    
-    
-    // let text = array.map((item) =>{
-    //     if(item[1] === true) return `${item[0]},`
-    //     else return ' '
-    // })
+    return filterString
+    };
+        
 
 const handleSubmit = (e) => {
-    console.log(region)
     e.preventDefault();
     getRecipes();
 }
-// + `intolerances=${intoleranceOption.map((item => `${item},`))}&`
+
 
 const getRecipes = async () => {
     const apiKey = 'd9621eb8bc7949e39c16d7f5fab52053';
     try {
         const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}`
-        + `&query=${search}&cuisine=${regionOption}&`
-        + `type=${mealTypeOption}&`
-        + `diet=${diets}&`
-        + `intolerances=${""}&`
+        + `&query=${search}&cuisine=${PrintFilters(region)}&`
+        + `type=${PrintFilters(mealType)}&`
+        + `diet=${PrintFilters(diet)}&`
+        + `intolerances=${PrintFilters(intolerance)}&`
         + `addRecipeInformation=true&addRecipeNutritionadd=true&fillIngredients=true&number=100`;
         const response = await fetch(url);
         const result = await response.json();
@@ -61,12 +59,12 @@ const getRecipes = async () => {
         
         <section className="search-form">
             <img src={logoimage} alt="global cuisine" className='logoimage'></img>
-            <form className="search-form__container" onSubmit={handleSubmit}>
+            <Form className="search-form__container" onSubmit={handleSubmit}>
                 <input className="search-form__input" onChange={(e) => setSearch(e.target.value)} maxLength={25} data-="text" placeholder="Search..."/>
                 <FaSearch className="search-form__submit" />
                 
                 
-            </form>
+            </Form>
             <FilterMenu region={region} mealType={mealType} diet={diet} intolerance={intolerance} />
         </section>
     
