562161256e6e12fde7664474d9d56363b610dffa
Luca Ionut-Catalin <ic.luca@outlook.com>
2023-05-02 12:15:17 +0200

    fixed fetch for recipe and similars using id

--- Global_Cuisine_Project/src/components/RecipeView.jsx
+++ Global_Cuisine_Project/src/components/RecipeView.jsx
@@ -1,46 +1,94 @@
 
 import './RecipeView.css'
 import recipeImage from '../assets/brooke.jpg'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useParams } from 'react-router-dom'
 import { useState,useEffect } from 'react';
 import parse from 'html-react-parser'
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import "@splidejs/splide/dist/css/splide.min.css";
+import { Link } from 'react-router-dom'
 
-
-export default function RecipeView() {
-    const location = useLocation();
-    const data = location.state.recipe;  //gets the recipe object from home page
-    const summary = '<h3>'+data.summary.split('. ',1)+'</h3>'; // splits and saves the first sentence of summary atribute of the recipe
-    
-
-    const [similar, setSimilars] = useState([]);
+const Similars = () => {
+const params = useParams();
+    const [similars, setSimilars] = useState([]);
     const getSimilars = async (id) => {
-    const apiKey = '27bb6d5c926f4d7a9031e952cb4c9849';
+    const apiKey = 'f27d562bd85b4cd5a482eb0b9108beeb';
     try {
         const url = `https://api.spoonacular.com/recipes/${id}/similar?apiKey=${apiKey}`;
         const response = await fetch(url);
         const result = await response.json();               
-        setSimilars(result.results);
+        setSimilars(result);
+        console.log(result);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+      useEffect(() => {        
+        getSimilars(params.recipeId)
+    },[params.recipeId])
+    return (
+        similars ?
+        <>
+        <h3>Similars:</h3>
+                
+                    <ul className='list'>
+                            {similars.map((similar) =>
+                                
+                                <li key={similar.id} className='list-element'>
+                                    <Link to={"/recipes/" +similar.id}>
+                                        {similar.title}</Link></li>
+                                
+                                
+                                
+                            )}                        
+                        
+                        
+                    
+               
+                </ul>
+                </> : <></>
+    )
+}
+
+export default function RecipeView() {
+    // const location = useLocation();
+    // const data = location.state.recipe;  //gets the recipe object from home page
+    const params = useParams();
+    console.log(params)
+    const [recipe,setRecipe] =useState();
+    const getRecipe = async (id) => {
+    const apiKey = '27bb6d5c926f4d7a9031e952cb4c9849';
+    try {
+        const url = `https://api.spoonacular.com/recipes/${id}/information?apiKey=${apiKey}&includeNutrition=true`;
+        const response = await fetch(url);
+        const result = await response.json();               
+        setRecipe(result);
         console.log(result);
       } catch (e) {
         console.log(e);
       }
     }
 
+  
+
     useEffect(() => {
-        getSimilars(data.id)
-    },[])
-    console.log(similar)
+        getRecipe(params.recipeId)
+        //getSimilars(params.recipeId)
+    },[params.recipeId])
+    //console.log(similar)
+    const summary = '<h3>'+recipe?.summary.split('. ',1)+'</h3>'; // splits and saves the first sentence of summary atribute of the recipe
+    
 
         return (
-            
+            recipe ?
             <div className="recipe-container">
                 <div className='recipe-info'>
                            
                     <div className="title-container"> 
-                        <h1>{data.title}</h1>
+                        <h1>{recipe.title}</h1>
                         <div className='title-info'>                       
                             <div className='image-container'>                   
-                            <img src={data.image} alt="recipe image" 
+                            <img src={recipe.image} alt="recipe image" 
                             className='recipe-img' ></img>
                             </div>
                             <div className='summary'>                            
@@ -48,15 +96,15 @@ export default function RecipeView() {
                                 <div>
                                     <div>
                                         <h5>Time to prepare</h5>
-                                        <h4>{data.readyInMinutes} minutes</h4>
+                                        <h4>{recipe.readyInMinutes} minutes</h4>
                                     </div> 
                                     <div>
                                         <h5>Servings</h5>
-                                        <h4>{data.servings} portions</h4>
+                                        <h4>{recipe.servings} portions</h4>
                                     </div> 
                                     <div>
                                         <h6>Health score</h6>
-                                        <h4 className='last'>{data.healthScore} points</h4>
+                                        <h4 className='last'>{recipe.healthScore} points</h4>
                                     </div>   
                                 </div>                            
                             </div>
@@ -71,7 +119,7 @@ export default function RecipeView() {
                         <hr className='solid' />           
                     <div>
                         <ul className='list'>
-                            {data.extendedIngredients.map((ingredient) =>
+                            {recipe.extendedIngredients.map((ingredient) =>
                                 <li key={ingredient.id} className='list-element'>{ingredient.original}</li>
                             )}                        
                         </ul>
@@ -82,7 +130,7 @@ export default function RecipeView() {
                         <hr className='solid' />            
                     <div>
                         <ol className='list'>
-                            {data.analyzedInstructions.map((instruction,index) => {
+                            {recipe.analyzedInstructions.map((instruction,index) => {
                                 return <div key={index} >
                                     {instruction.steps.map((step) => <li key={step.number}>
                                         <span>{step.step}</span></li>)}
@@ -94,37 +142,43 @@ export default function RecipeView() {
                     </div>
                 </div>                
             </div>
+            <div className='recipe-ingredients'>
+                <Similars props={recipe.id}></Similars>
+                
+            </div>
+            
             </div> 
+            <div>
                 <div className="nutrition-info">
-                    <h2>Nutritions Facts : </h2>
+                    <h3 className='nutrition-title'>Nutritions Facts : </h3>
                     <hr className='solid'></hr>                    
                     <p className="serving">Serving size : <span>
-                        {data.nutrition.weightPerServing.amount}{data.nutrition.weightPerServing.unit}</span></p>
+                        {recipe.nutrition.weightPerServing.amount}{recipe.nutrition.weightPerServing.unit}</span></p>
                         <hr className="light"/>
                         <p className="caloric">Caloric breakdown: </p>
                         <div className="caloricinfo">
                         <p>Carbs: <span>
-                        {data.nutrition.caloricBreakdown.percentCarbs}%</span></p>
+                        {recipe.nutrition.caloricBreakdown.percentCarbs}%</span></p>
                         <p>Fat: <span>
-                        {data.nutrition.caloricBreakdown.percentFat}%</span></p>
+                        {recipe.nutrition.caloricBreakdown.percentFat}%</span></p>
                         <p>Protein: <span>
-                        {data.nutrition.caloricBreakdown.percentProtein}%</span></p>
+                        {recipe.nutrition.caloricBreakdown.percentProtein}%</span></p>
                         </div>
                         <hr className="light"/>
                         <div className="nutrients">
-                        {data.nutrition.nutrients.map((nutrient) => <p className="nutrient">{nutrient.name}: <span>
+                        {recipe.nutrition.nutrients.map((nutrient,index) => <p key={index} className="nutrient">{nutrient.name}: <span>
                         {nutrient.amount}{nutrient.unit}</span></p> )}
                         </div>
                         
                     
-                    <div >
-                        <ul>
-                              {/* {data.}   */}
-                            </ul>
-                    </div>
-                    
+                  
+                 </div>  
+                 {/* <div className='nutrition-info'>
+                    <h4>This meal goes with :</h4>
+                    {}
+                    </div>  */}
                 </div>
-            </div>
+            </div> : <></>
             
         )
    
