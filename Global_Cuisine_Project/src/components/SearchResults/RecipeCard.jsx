0675aaca8e4f141b45446c76d4378d0d7b655aae
Jonas Jonas <jonassaghi@gmail.com>
2023-05-12 10:26:09 +0200

    some tags,ready in

--- Global_Cuisine_Project/src/components/SearchResults/RecipeCard.jsx
+++ Global_Cuisine_Project/src/components/SearchResults/RecipeCard.jsx
@@ -5,15 +5,31 @@ import { Link
 
 const RecipeCard = (props) => {
     return (
-        <div className="recipe-card">
+      <div className="recipe-card">
           <Link to={"recipes/" + props.recipe.id}>
-          <img src={props.recipe.image} alt="Recipe Image"></img>
-          <div className="text-container">
-          <h1>{props.recipe.title}</h1>
-          </div>
+            <img src={props.recipe.image} alt="Recipe Image" ></img>
+            <div className="vegantag">
+              <div><h3>{props.recipe.vegan ? <Vegan /> : <p></p>}</h3></div>
+            </div>
+            <div className="text-container">
+              <h1>{props.recipe.title}</h1>
+            </div>
           </Link>
+          <div className="tags">
+              {/* <h3>{props.recipe.vegan ? <Vegan /> : <p></p>}</h3> */}
+              <div><p>Ready in </p>{props.recipe.readyInMinutes} minutes</div>
+          </div>
         </div>
     );
   };
   
   export default RecipeCard;
+
+
+  const Vegan =() => {
+    return <p>Vegan</p>
+  }
+
+  // const ReadyIn =() => {
+  //   return <p>Ready in +{props.recipe.readyInMinutes}</p>
+  // }
