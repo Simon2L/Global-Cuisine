49e8ee49985ea090e75025e90d5d7ead8b95dcc3
HarunMohamed2022 <112860947+HarunMohamed2022@users.noreply.github.com>
2023-05-14 21:49:17 +0200

    Added Hover and links to diffrent website

--- Global_Cuisine_Project/src/components/Footer/Footer.jsx
+++ Global_Cuisine_Project/src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./Footer.css"
-import {BsYoutube, BsTwitter, BsInstagram, BsLinkedin} from "react-icons/bs"
+import {BsYoutube, BsTwitter, BsInstagram, BsLinkedin, BsGithub} from "react-icons/bs"
 
 const Footer = () => {
   return (
@@ -9,47 +9,45 @@ const Footer = () => {
             <div className='sb__footer-links'>
                 <div className='sb__footer-links_div'>
                     <h4>For Business</h4>
-                    <a href="#">
+                    <a href="/theTeam">
                         <p>Employer</p>
                     </a>
-                    <a href="#">
+                    <a href="https://en.wikipedia.org/wiki/Health_insurance" target="_blank">
                         <p>Health Plan</p>
                     </a>
-                    <a href="#">
-                        <p>Individual</p>
-                    </a>
+                   
                 </div>
                 <div className='sb__footer-links_div'>
                     <h4>Help</h4>
                     
-                    <a href="#">
+                    <a href="/contact">
                         <p>Contact us</p>
                     </a>
                     
                 </div>
                 <div className='sb__footer-links_div'>
                     <h4>Parters</h4>
-                    <a href="#">
+                    <a href="https://spoonacular.com/" target="_blank">
                         <p>Spoonacular</p>
                     </a>
                 </div>
                 <div className='sb__footer-links_div'>
                     <h4>About Us</h4>
-                    <a href="#">
+                    <a href="/about">
                         <p>Summary</p>
                     </a>
-                    <a href="#">
+                    <a href="/theTeam">
                         <p>Team</p>
                     </a>
                     
                 </div>
                 <div className='socialmedia'>
-                    <h4>Cooming soon</h4>
+                    <h4>Social Platform</h4>
                     <div className='socialmedia-Icon'>
-                    <a href="#" ><BsYoutube></BsYoutube></a>
-                    <a href="#" ><BsTwitter></BsTwitter></a>
-                    <a href="#" ><BsInstagram></BsInstagram></a>
-                    <a href="#" ><BsLinkedin></BsLinkedin></a>
+                    <a href="https://www.youtube.com/watch?v=_ZXeFPpPJeI&ab_channel=ChuckDrake" target="_blank"><BsYoutube></BsYoutube></a>
+                    <a href="https://twitter.com/" target="_blank"><BsTwitter></BsTwitter></a>
+                    <a href="https://www.instagram.com/" target="_blank"><BsInstagram></BsInstagram></a>
+                    <a href="https://github.com/Simon2L/Global-Cuisine" target="_blank" ><BsGithub></BsGithub></a>
                     </div>
                 </div>
             </div>
@@ -64,10 +62,10 @@ const Footer = () => {
             </p>
         </div>
         <div className='sb__footer-below-links'>
-            <a href="#"><div><p>Terms & Condition</p></div></a>
-            <a href="#"><div><p>Privacy</p></div></a>
-            <a href="#"><div><p>Security</p></div></a>
-            <a href="#"><div><p>Cookies Declaration</p></div></a>
+            <a href="https://en.wikipedia.org/wiki/Terms_of_service" target="_blank"><div><p>Terms & Condition</p></div></a>
+            <a href="https://en.wikipedia.org/wiki/Privacy_policy" target="_blank"><div><p>Privacy</p></div></a>
+            <a href="https://en.wikipedia.org/wiki/Security_policy" target="_blank"><div><p>Security</p></div></a>
+            <a href="https://en.wikipedia.org/wiki/HTTP_cookie" target="_blank"><div><p>Cookies Declaration</p></div></a>
         </div>
     </div>
 
