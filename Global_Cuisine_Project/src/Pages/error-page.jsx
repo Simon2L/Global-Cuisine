73b7933fb3bb70171a931871255d0af3d710a39f
HarunMohamed2022 <112860947+HarunMohamed2022@users.noreply.github.com>
2023-05-17 15:40:43 +0200

    Added Small screen version, Button

--- Global_Cuisine_Project/src/Pages/error-page.jsx
+++ Global_Cuisine_Project/src/Pages/error-page.jsx
@@ -1,5 +1,6 @@
 import { useRouteError } from "react-router-dom";
 import "./error-page.css";
+import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
@@ -7,14 +8,16 @@ export default function ErrorPage() {
 
   return (
     <div  >
-      <div className="Error-page" >
+      <div className="bg">
+        <div className="Error-page">
+          <h1 className="Ooops-container">Oooops!!! Something Went Wrong</h1>
+          <p>
+            <i className="error-text">{error.statusText || error.message}</i>
+          </p>
+          <Link to="/"><button className="btn">HOME</button></Link>
+        </div>
+      </div>
+    </div>
 
-      
-      <h1 className="Ooops-container">Oooops!!! Something Went Wrong</h1>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
-    </div>
   );
 }
