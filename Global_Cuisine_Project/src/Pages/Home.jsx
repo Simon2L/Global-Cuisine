1a181c4070ba92e2e1399e014ab5914222ea3351
Jonas Jonas <jonassaghi@gmail.com>
2023-05-19 10:38:17 +0200

    bytte namn på knappen

--- Global_Cuisine_Project/src/Pages/Home.jsx
+++ Global_Cuisine_Project/src/Pages/Home.jsx
@@ -76,7 +76,7 @@ const BackToTopBtn = () => {
   }
 
   return (
-    <button id="top-btn" onClick={() => topFunction()}>Back to search</button>
+    <button id="top-btn" onClick={() => topFunction()}>Back to top</button>
     
   )
 }      
